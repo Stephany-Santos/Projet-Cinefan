eae2c2207a8345c1eef9af989fdb05ade769c928
nik1-ie <niekitaj0805@gmail.com>
2025-12-05 21:37:44 +0100

    edits

--- projet/db.py
+++ projet/db.py
@@ -3,25 +3,12 @@ import psycopg2.extras
 
 def connect():
 	conn = psycopg2.connect(
-		dbname = "niekita;joseph",
+		dbname = "postgres",
+        user = "postgres",
+        password = "mdp",
 		cursor_factory = psycopg2.extras.NamedTupeCursor
 	)
 	conn.autocommit = True
 	return conn
 
-# Fichier dans lequel on pourra créer des fonctions filtres (tout nos select etc) à afficher sur le site
 
-	 
-@app.route("/listemedia")
-def listemedia():
-    with db.connect() as conn:
-        with conn.cursor() as cur:
-            cur.execute("""
-                SELECT m.id_media, m.titre, m.parution, m.genre, m.type,
-                       a.nom AS real_nom, a.prenom AS real_prenom
-                FROM media m
-                JOIN artiste a ON m.realise = a.id_artiste
-                ORDER BY m.parution DESC
-            """)
-            result = cur.fetchall()
-    return render_template("medialiste.html", medialiste=result)
