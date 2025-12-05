33ccfd0961474cad49c91c07f92455035900abce
florergn <florerigoigne@gmail.com>
2025-12-05 17:41:02 +0100

    Update main.py

--- projet/main.py
+++ projet/main.py
@@ -19,20 +19,3 @@ app = Flask(__name__)
 if __name__ == '__main__':
     app.run()
 
-@app.route("/listemedia")
-def listemedia():
-    # Connexion à la base et récupération des médias
-    conn = db.connect()
-    cur = conn.cursor()
-    cur.execute("""
-        SELECT m.id_media, m.titre, m.parution, m.genre, m.type,
-               a.nom AS real_nom, a.prenom AS real_prenom
-        FROM media m
-        JOIN artiste a ON m.realise = a.id_artiste
-        ORDER BY m.parution DESC
-    """)
-    result = cur.fetchall()
-    cur.close()
-    conn.close()
-
-    return render_template("medialiste.html", medialiste=result)
