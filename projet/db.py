e009ebac00fc7caefdc6e0dfb513a4d35c498b4d
florergn <florerigoigne@gmail.com>
2025-12-05 17:29:45 +0100

    Update db.py

--- projet/db.py
+++ projet/db.py
@@ -12,16 +12,4 @@ def connect():
 # Fichier dans lequel on pourra créer des fonctions filtres (tout nos select etc) à afficher sur le site
 
 	 
-@app.route("/listemedia")
-def listemedia():
-    with db.connect() as conn:
-        with conn.cursor() as cur:
-            cur.execute("""
-                SELECT m.id_media, m.titre, m.parution, m.genre, m.type,
-                       a.nom AS real_nom, a.prenom AS real_prenom
-                FROM media m
-                JOIN artiste a ON m.realise = a.id_artiste
-                ORDER BY m.parution DESC
-            """)
-            result = cur.fetchall()
-    return render_template("medialiste.html", medialiste=result)
+
