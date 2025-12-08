8e56ab6907c33b5daf9833b445884df6fbb79014
nik1-ie <niekitaj0805@gmail.com>
2025-12-08 11:13:25 +0100

    merging

---
+++ projet/static/filtres.py
@@ -0,0 +1,39 @@
+import psycopg2
+import psycopg2.extras
+import db
+
+def critiques_per_user(user):
+    '''
+    Récupère le nombre de critiques écrites par chaque membre du club.
+    Arguments:
+        user (str): Le pseudo de l'utilisateur
+    Return:
+        int: Le nombre de critiques écrites par l'utilisateur
+    '''
+    with db.connect as conn:
+        with conn.cursor() as cur:
+            cur.execute('SELECT utilisateur.pseudo, COUNT(commente.utilisateur) AS nombreDeCritiques FROM utilisateur LEFT JOIN commente ON utilisateur.pseudo = commente.utilisateur WHERE utilisateur = %s GROUP BY utilisateur.pseudo', (user,))
+            resultat = cur.fetchone()
+    return resultat[0]
+
+
+# -- Pour chaque acteur, le nombre de films de chaque genre dans lesquels il a joué,
+# -- trié par nombre de films descendant.
+# CREATE VIEW filmsParActeur AS (
+#     SELECT a.nom, a.prenom, m.genre, COUNT(*) AS nombreDeFilms
+#     FROM artiste a
+#     JOIN participe p ON p.id_artiste = a.id
+#     JOIN media m ON m.id_media = p.id_media
+#     GROUP BY a.nom, a.prenom, m.genre;
+# )
+
+
+# -- Le nombre moyen de critiques écrites par utilisateur pour chaque genre.
+
+# CREATE VIEW moyenneCritiques AS (
+#     SELECT media.genre, AVG(nbCritiques.nombreDeCritiques) AS moyenneCritiquesParGenre
+#     FROM media
+#     JOIN commente ON media.id_media = commente.id_media
+#     JOIN nbCritiques ON commente.utilisateur = nbCritiques.pseudo
+#     GROUP BY media.genre
+# )
--- projet/static/getdata.py
+++ projet/static/getdata.py
@@ -24,31 +24,3 @@ if __name__ == "__main__":
     print(media("Inception"))
 
 
-# -- Pour chaque acteur, le nombre de films de chaque genre dans lesquels il a joué,
-# -- trié par nombre de films descendant.
-# CREATE VIEW filmsParActeur AS (
-#     SELECT a.nom, a.prenom, m.genre, COUNT(*) AS nombreDeFilms
-#     FROM artiste a
-#     JOIN participe p ON p.id_artiste = a.id
-#     JOIN media m ON m.id_media = p.id_media
-#     GROUP BY a.nom, a.prenom, m.genre;
-# )
-
-# -- le nombre de critiques écrites par chaque membre du club.
-
-# CREATE VIEW nbCritiques AS (
-#     SELECT utilisateur.pseudo, COUNT(commente.utilisateur) AS nombreDeCritiques
-#     FROM utilisateur
-#     LEFT JOIN commente ON utilisateur.pseudo = commente.utilisateur
-#     GROUP BY utilisateur.pseudo
-# )
-
-# -- Le nombre moyen de critiques écrites par utilisateur pour chaque genre.
-
-# CREATE VIEW moyenneCritiques AS (
-#     SELECT media.genre, AVG(nbCritiques.nombreDeCritiques) AS moyenneCritiquesParGenre
-#     FROM media
-#     JOIN commente ON media.id_media = commente.id_media
-#     JOIN nbCritiques ON commente.utilisateur = nbCritiques.pseudo
-#     GROUP BY media.genre
-# )
