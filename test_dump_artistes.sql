161bd27f63c2703caeed9058ce3a90a5a95116ce
florergn <florerigoigne@gmail.com>
2025-12-10 06:44:00 +0100

    Update test_dump_artistes.sql

--- test_dump_artistes.sql
+++ test_dump_artistes.sql
@@ -1,1 +1,8 @@
 
+
+CREATE TABLE artiste (
+    id_artiste SERIAL PRIMARY KEY,
+    nom VARCHAR(100),
+    prenom VARCHAR(100) NOT NULL,
+    cree_par VARCHAR(20) DEFAULT 'deleted-user' REFERENCES utilisateur(pseudo) ON DELETE SET DEFAULT NOT NULL
+);
