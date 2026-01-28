48b99d355f64568d4818094b713a7d37b97a2d9f
nik1-ie <niekitaj0805@gmail.com>
2026-01-28 19:35:35 +0100

    typo

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ def connect():
   conn = psycopg2.connect(
     # ---- using remote database
     host="dpg-d5sv4j75r7bs73bai5l0-a",
-    dbname="cinefan-db",
+    dbname="cinefandb",
     user="cinefandb_user",
     password="kRGteTX0EQ4caE4XXZw2SeXMdx5yJy0A",
     port=5432,
