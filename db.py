56fee4ff250afc8de169894a19f302629f3a6949
nik1-ie <niekitaj0805@gmail.com>
2026-01-28 15:20:04 +0100

    editing db connection

--- db.py
+++ db.py
@@ -1,11 +1,15 @@
 import psycopg2
 import psycopg2.extras
+import os
 
 def connect():
   conn = psycopg2.connect(
-    dbname = 'postgres',
-    user = 'postgres',
-    password = "",
+    # ---- using remote database
+    url = os.environ["CINEFANDATABASE_URL"]
+    # ---- using local database
+    # dbname = 'postgres',
+    # user = 'postgres',
+    # password = "",
     cursor_factory = psycopg2.extras.NamedTupleCursor
   )
   conn.set_client_encoding('UTF8')
